a0ef36f5907ab6b45926aeca842533210ef892c8
Yoel Augustan <yoelagustan@gmail.com>
2026-03-10 22:56:59 +0700

    Update Migration

---
+++ sttb-backend-api/sttb.Entities/Migrations/20260310155515_AddExcerptFeatured_EventCategory_MediaCategoryTag.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sttb.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExcerptFeatured_EventCategory_MediaCategoryTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Media",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Media",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Media");
+
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Media");
+        }
+    }
+}
